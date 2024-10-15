21813e2a7966d6eb166250b505516b1a973d2dc6
tony2-5 <antdvory@gmail.com>
2024-10-15 12:41:20 -0400

    Implemented API endpoints

--- src/index.js
+++ src/index.js
@@ -8,9 +8,56 @@ app.use(express.json());
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+let userData = []
+let id = 0
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/users', (req, res) => {
+    userData.push({"id": id,...req.body})
+    const user = userData.find(user => user.id == id)
+    res.status(201).send(user)
+    id++
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = userData.find(user => user.id == id)
+    if(user) {
+        res.status(200).send(user)
+    } else {
+        res.status(404).send("User not found")
+    }
+});
+
+app.put('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = userData.find(user => user.id == id)
+    if(user) {
+        if(req.body.email && req.body.name) {
+            user.name = req.body.name
+            user.email = req.body.email
+            res.status(200).send(user)
+        } else if(req.body.name) {
+            user.name = req.body.name
+            res.status(200).send(user)
+        } else if(req.body.email) {
+            user.email = req.body.email
+            res.status(200).send(user)
+        }
+        res.status(400).send("No update parameters passed")
+    } else {
+        res.status(404).send("User not found")
+    }
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = userData.find(user => user.id == id)
+    if(user) {
+        userData = userData.filter(user => user.id != id)
+        res.status(204).send()
+    } else {
+        res.status(404).send("User not found")
+    }
 });
 
 // Do not touch the code below this comment
